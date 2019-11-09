a01b9bf0add2b6e66c503f4cd3109c8681f4d10d
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-11-08 22:44:29 -0200

    Arrumado conflito

--- src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
@@ -10,13 +10,8 @@ import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.ProdutosRespository;
 
 @Controller
 public class HomeController{
-<<<<<<< HEAD
     @GetMapping("/")
     public String list(ProdutosRespository repository){
-=======
-   @GetMapping("/")
-   public String index(Model model){
->>>>>>> cb3eda79c340ec1a9aa65ba8e411a0cf225af394
         return "comum/index";
     }
    @GetMapping(value = "/search", params = {"pesquisar"})
