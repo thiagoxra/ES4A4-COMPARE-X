b7fb5aa15249698669c6a1c60d00360493c24971
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-11-10 04:21:37 -0300

    Criado em homecontroller começo para conseguir passar nos primeiros testes

--- src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
@@ -1,22 +1,37 @@
 package br.edu.ifsp.lp2a2.comparex.comum.controllers;
 
-
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.Produto;
 import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.ProdutosRespository;
 
-
-
 @Controller
-public class HomeController{
+public class HomeController {
+    @Autowired
+    private ProdutosRespository repository;
     @GetMapping("/")
-    public String list(ProdutosRespository repository){
-        return "comum/index";
+    public ModelAndView list(){
+        ModelAndView mv = new ModelAndView("comum/index");
+        ArrayList<Produto> produtos = new ArrayList<Produto>();
+        repository.findAll().forEach(produtos::add);
+
+
+        mv.addObject("produtos", produtos);
+        return mv;
     }
    @GetMapping(value = "/search", params = {"pesquisar"})
    public String search(String pesquisar){
 
        return "comum/resultado_busca";
+   }
+   /**
+    * @return the repository
+    */
+   public ProdutosRespository getRepository() {
+       return repository;
    }
 }
