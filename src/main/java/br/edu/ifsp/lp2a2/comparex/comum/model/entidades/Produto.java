3a4cbe2bcf9d9a6a22205047a6274e5fe3a23a88
Thiago Xavier <thiago.xra@gmail.com>
2019-11-22 23:19:25 -0200

    Criei atributos para o arquivo Produto.java

--- src/main/java/br/edu/ifsp/lp2a2/comparex/comum/model/entidades/Produto.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comum/model/entidades/Produto.java
@@ -11,6 +11,16 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String descricao;
+    private String nome;
+    private double preco;
+    private String unid;
+    private String marca;
+    private String imagem;
+    private String acabamento;
+    private String categoria;
+    private String embalagem;
+    private String cor;
+    
 
     public Long getId() {
         return id;
@@ -26,5 +36,77 @@ public class Produto {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getUnid() {
+        return unid;
+    }
+
+    public void setUnid(String unid) {
+        this.unid = unid;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getImagem() {
+        return imagem;
+    }
+
+    public void setImagem(String imagem) {
+        this.imagem = imagem;
+    }
+
+    public String getAcabamento() {
+        return acabamento;
+    }
+
+    public void setAcabamento(String acabamento) {
+        this.acabamento = acabamento;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getEmbalagem() {
+        return embalagem;
+    }
+
+    public void setEmbalagem(String embalagem) {
+        this.embalagem = embalagem;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
     }
 }
