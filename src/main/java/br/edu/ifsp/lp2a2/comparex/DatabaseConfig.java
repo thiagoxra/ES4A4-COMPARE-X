171f486c0cf6b1d7e7b3590776ae0151bc175427
Vinicius Eustaquio Cortez <vinicius@buscaprev.com.br>
2019-12-06 16:37:29 -0300

    Test

--- src/main/java/br/edu/ifsp/lp2a2/comparex/DatabaseConfig.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/DatabaseConfig.java
@@ -1,6 +1,7 @@
 package br.edu.ifsp.lp2a2.comparex;
 
 
+
 import com.zaxxer.hikari.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
@@ -9,13 +10,19 @@ import javax.sql.DataSource;
 @Configuration
 public class DatabaseConfig {
 
-  @Value("${spring.datasource.url}")
+  @Value("${DATABASE_JDBC}")
   private String dbUrl;
+  @Value("${DATABASE_USER}")
+  private String username;
+  @Value("${DATABASE_PASSWORD}")
+  private String password;
 
   @Bean
   public DataSource dataSource() {
       HikariConfig config = new HikariConfig();
-      config.setJdbcUrl(dbUrl);
+      config.setJdbcUrl(dbUrl+"?user="+username+"&password="+password+"&ssl=true");
+      config.setUsername(username);
+      config.setPassword(password);
       return new HikariDataSource(config);
   }
 }
