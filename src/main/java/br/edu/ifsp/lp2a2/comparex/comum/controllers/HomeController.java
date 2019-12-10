87662a76e233cce27cf70ad855c5b2d282feaf6a
rafaelpeinado <rafael.peinado.silva@gmail.com>
2019-12-09 22:39:16 -0300

    buscar funcionando

--- src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
@@ -1,18 +1,8 @@
 package br.edu.ifsp.lp2a2.comparex.comum.controllers;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.Produto;
 import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.ProdutosRespository;
 /*import br.edu.ifsp.lp2a2.comparex.comum.services.ComparadorProdutosService;*/
 
@@ -24,48 +14,16 @@ private ProdutosRespository repository;
     public HomeController(ProdutosRespository repository){
         this.repository = repository;
     }
-
    
     @GetMapping("/")
 	public String index(Model model) {
- 
 		/*model.addAttribute("produtos", repository.findAll()); */
-		
 		return "comum/index";
 	}
 
-   @PostMapping("/search")
-   public String resultado(Model model) {
-	   model.addAttribute("produtos", repository.findAll()); 
+   @GetMapping(value = "/search", params = {"pesquisar"})
+   public String resultado(Model model, String pesquisar) {
+	   model.addAttribute("produtos", repository.findByNomeContaining(pesquisar)); 
 		return "comum/resultado";
 	}
-	
-	
-	
-    /*public HomeController(ProdutosRespository repository){
-        this.repository = repository;
-    }
-
-
-
-    private ProdutosRespository repository;
-    @GetMapping("/")
-    public ModelAndView list(){
-        ModelAndView mv = new ModelAndView("comum/index");
-        ComparadorProdutosService comparador = new ComparadorProdutosService(repository);
-        // Deve ser optimizado
-        mv.addObject("produtos", comparador.comparadorPontuacao(5));
-        return mv;
-    }
-   @GetMapping(value = "/search", params = {"pesquisar"})
-   public String search(String pesquisar){
-
-       return "comum/resultado";
-   }*/
-   /**
-    * @return the repository
-    */
-   /*public ProdutosRespository getRepository() {
-       return repository;
-   }*/
 }
--- src/main/java/br/edu/ifsp/lp2a2/comparex/comum/model/entidades/ProdutosRespository.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comum/model/entidades/ProdutosRespository.java
@@ -1,5 +1,7 @@
 package br.edu.ifsp.lp2a2.comparex.comum.model.entidades;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ public interface ProdutosRespository extends CrudRepository<Produto, Long> {
     // É assim que se faz uma query se for necessário
     // @Query("SELECT p FROM Produto p ORDER BY pontuacao DESC")
     // Iterable<Produto> findByPontuacaoOrder();
+	List<Produto> findByNomeContaining(String nome);
 }
