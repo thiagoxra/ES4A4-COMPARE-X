cc65dcfeb5b5b0b2d124ae0a77afbb072e84fae1
Vinicius Eustaquio Cortez <viniciusecortez@outlook.com>
2019-12-10 13:07:26 -0300

    Arrumado conjunto de testes

--- src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comum/controllers/HomeController.java
@@ -3,8 +3,10 @@ package br.edu.ifsp.lp2a2.comparex.comum.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.ProdutosRespository;
-/*import br.edu.ifsp.lp2a2.comparex.comum.services.ComparadorProdutosService;*/
+
 
 @Controller
 public class HomeController {
@@ -16,14 +18,14 @@ private ProdutosRespository repository;
     }
    
     @GetMapping("/")
-	public String index(Model model) {
-		/*model.addAttribute("produtos", repository.findAll()); */
+	public String index() {
 		return "comum/index";
 	}
 
    @GetMapping(value = "/search", params = {"pesquisar"})
-   public String resultado(Model model, String pesquisar) {
-	   model.addAttribute("produtos", repository.findByNomeContaining(pesquisar)); 
-		return "comum/resultado";
+   public ModelAndView resultado(String pesquisar) {
+	   ModelAndView mv = new ModelAndView("comum/resultado");
+	   mv.addObject("produtos", repository.findByNomeContaining(pesquisar)); 
+	   return mv;
 	}
 }
--- src/test/java/br/edu/ifsp/lp2a2/comparex/unitarios/controller/HomeControllerTest.java
+++ src/test/java/br/edu/ifsp/lp2a2/comparex/unitarios/controller/HomeControllerTest.java
@@ -1,47 +1,49 @@
-/*package br.edu.ifsp.lp2a2.comparex.unitarios.controller;
+package br.edu.ifsp.lp2a2.comparex.unitarios.controller;
 
 import br.edu.ifsp.lp2a2.comparex.comum.controllers.HomeController;
 import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.Produto;
 import br.edu.ifsp.lp2a2.comparex.comum.model.entidades.ProdutosRespository;
-import br.edu.ifsp.lp2a2.comparex.comum.services.ComparadorProdutosService;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
 import org.mockito.Mockito;
-import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
 
 public class HomeControllerTest {
     ProdutosRespository repository;
+    HomeController homeController;
+    List<Produto> list;
     public HomeControllerTest(){
         this.repository = Mockito.mock(ProdutosRespository.class);
+        this.homeController = new HomeController(repository);
+        this.list = new ArrayList<>();
+        this.list.add(new Produto(1, "Massa"));
+        this.list.add(new Produto(2, "Cimento"));
     }
     @Test
-    public void index_return_template() {
-        // Configuracao
-        HomeController controller = new HomeController(this.repository);
+    public void resultado_right_address_resultTest(){
+        ModelAndView actualmv = homeController.resultado("massa");
+        String actual = actualmv.getViewName();
+        String expected = "comum/resultado";
+        assertEquals(expected , actual);
+    }
+    @Test
+    public void resultado_objects_not_null(){
+        assertNotNull(homeController.resultado("Massa").getModel().get("produtos"));
+    }
+
+    @Test
+    public void index_right_address(){
         String expected = "comum/index";
-        // Execução
-        String actual = controller.list().getViewName();
-        //Comparacao
+        String actual = homeController.index();
         assertEquals(expected, actual);
     }
-
-    @Test
-    public void index_return_model(){
-        // Configuração
-        HomeController controller = new HomeController(repository);
-        ComparadorProdutosService comp = new ComparadorProdutosService(repository);
-        Map<String, Object> expected = new HashMap();
-        // Execução
-        expected.put("produtos", comp.comparadorPontuacao(5));
-        Map<String, Object> actual  = controller.list().getModel();
-        // Comparação
-        assertEquals(expected, actual);
-    }
-
-}*/
+}
