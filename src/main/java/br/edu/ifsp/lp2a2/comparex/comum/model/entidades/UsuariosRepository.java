f94efa7c52f4ca780c245468299627bd49943ca1
larissaCarvalho23 <carvalholb95@hotmail.com>
2019-12-17 17:18:38 -0300

    Criado o cadastro de usuario

--- src/main/java/br/edu/ifsp/lp2a2/comparex/comum/model/entidades/UsuariosRepository.java
+++ src/main/java/br/edu/ifsp/lp2a2/comparex/comum/model/entidades/UsuariosRepository.java
@@ -19,8 +19,8 @@ public interface UsuariosRepository extends CrudRepository<Usuario, Integer> {
 //  @Query("select new br.edu.ifsp.lp2a2.comparex.comum.model.entidades.Usuario(u.id, u.nome, u.senha, u.email) from Usuario u where u.email = login and u.senha = senha")
 //  Usuario findByUser(@Param("login") String login,@Param("senha") String senha);
 //
-//    <T> Optional<T> findById(int id);
-//    Usuario findUserById(int id);
+   <T> Optional<T> findById(int id);
+   Usuario findUserById(int id);
 ////    @Query("select new br.edu.ifsp.lp2a2.comparex.comum.model.entidades.ResultadoLojaProduto(lp.idProduto, count(lp.idLoja)) from LojaProduto lp group by lp.idProduto")
 ////    List<ResultadoLojaProduto> quantidadeLojas();
 
